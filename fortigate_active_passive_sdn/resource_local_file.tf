160ab484a3fb7553126ad8a7f947a1eaad8fd349
John McDonough <movinalot@gmail.com>
2023-07-05 17:10:38 -0400

    formatting

--- fortigate_active_passive_sdn/resource_local_file.tf
+++ fortigate_active_passive_sdn/resource_local_file.tf
@@ -2,5 +2,5 @@ resource "local_sensitive_file" "tempalte_file" {
   for_each = local.virtual_machines
 
   filename = format("fortios_%s.cfg", each.value.name)
-  content = local.virtual_machines[each.key].os_profile_custom_data
+  content  = local.virtual_machines[each.key].os_profile_custom_data
 }
